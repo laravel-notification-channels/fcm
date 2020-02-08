6114c165551fffbda8b4dbdc35447258854a4ebd
Chris Bautista <chris.bautista@coreproc.ph>
2020-02-09 01:40:21 +0800

    wip

---
+++ src/Resources/FcmResource.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NotificationChannels\Fcm\Resources;
+
+interface FcmResource
+{
+    /**
+     * @return array
+     */
+    public function toArray(): array;
+}
---
+++ src/Resources/NotificationPriority.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NotificationChannels\Fcm\Resources;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self PRIORITY_UNSPECIFIED()
+ * @method static self PRIORITY_MIN()
+ * @method static self PRIORITY_LOW()
+ * @method static self PRIORITY_DEFAULT()
+ * @method static self PRIORITY_HIGH()
+ * @method static self PRIORITY_MAX()
+ */
+class NotificationPriority extends Enum
+{
+    //
+}
