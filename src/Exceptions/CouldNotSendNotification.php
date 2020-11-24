e63ee86a534f72746b64ec2dfe6d7b112a9541df
Ankur Kumar <ankurk91@users.noreply.github.com>
2020-11-25 03:06:47 +0530

    Add failed notification event (#64)

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,12 +2,23 @@
 
 namespace NotificationChannels\Fcm\Exceptions;
 
+use Exception;
 use Kreait\Firebase\Exception\MessagingException;
+use Kreait\Firebase\Messaging\Message;
 
-class CouldNotSendNotification extends \Exception
+class CouldNotSendNotification extends Exception
 {
-    public static function serviceRespondedWithAnError(MessagingException $messagingException)
+    public static function serviceRespondedWithAnError(MessagingException $exception)
     {
-        return new static($messagingException->getMessage());
+        return new static(
+            $exception->getMessage(),
+            $exception->getCode(),
+            $exception
+        );
+    }
+
+    public static function invalidMessage()
+    {
+        return new static('The toFcm() method only accepts instances of ' . Message::class);
     }
 }
